7b2d7536fdecaf7eaa04b40033936ab193816858
sayanjitdas <sayanjitgit@gmail.com>
2023-09-18 12:46:21 +0530

    commit to handle pagination exceptions

--- blongo/blog/views.py
+++ blongo/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
@@ -8,7 +8,14 @@ def post_list(request):
     posts = Post.published.all()
     paginator = Paginator(posts, 3)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
