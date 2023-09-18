d667675695a83962b1a1f19e69714d6d64b84b8d
sayanjitdas <sayanjitgit@gmail.com>
2023-09-18 11:52:08 +0530

    added canonical urls to blog details

--- blongo/blog/models.py
+++ blongo/blog/models.py
@@ -3,6 +3,7 @@ from typing import Any
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.query import QuerySet
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -22,7 +23,7 @@ class Post(models.Model):
         DRAFT = "DF", "draft"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -40,6 +41,17 @@ class Post(models.Model):
     class Meta:
         ordering = ["-publish"]
         indexes = [models.Index(fields=["-publish"])]
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
 
     def __str__(self):
         return self.title
