06c3e58611512507db329ab622fe7aa1b6e78b44
sayanjitdas <sayanjitgit@gmail.com>
2023-09-19 22:02:47 +0530

    added class based view to post_list

--- blongo/blog/views.py
+++ blongo/blog/views.py
@@ -1,22 +1,22 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
-
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get("page", 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # if page_number is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     paginator = Paginator(posts, 3)
+#     page_number = request.GET.get("page", 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # if page_number is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page_number is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -29,3 +29,11 @@ def post_detail(request, year, month, day, post):
         status=Post.Status.PUBLISHED,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+# -------------------------- Class based views ---------------------------------
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
